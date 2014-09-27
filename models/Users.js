61566b410cd07df2d6a1f8dcf7c4203582f5bad2
Rene Zhang <rz99@cornell.edu>
2014-09-27 12:24:48 -0400

    users tables

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+'use strict';
+(require('rootpath')());
+
+var db = require('config/orm');
+
+var Users = db.define('users', {
+  user_id       : { type: "serial", key: true }, // autoincrementing primary key
+  first_name    : { type: "text", size: 20, required: true },
+  last_name     : { type: "text", size: 20, required: true  },
+  email         : { type: "text", size: 45, required: true, unique: true},
+  password_hash : { type: "text", size: 60, required: true },
+}, {
+  methods : {
+
+  }
+});
+
+Users.sync( function(err) {
+  !err && console.log("Synced users!");
+});
+
+module.exports = Users;
