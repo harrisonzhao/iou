1aba01f874cea9a0b6ec345e2f8f413a2d1a2daf
Harrison Zhao <roarofpie@gmail.com>
2014-09-27 12:22:46 -0400

    update

--- config/passport.js
+++ config/passport.js
@@ -1,63 +1,69 @@
-'use strict';
+/*'use strict';
 require('rootpath')();
 
-//OAuth strategy dependencies
 var LocalStrategy = require('passport-local').Strategy;
-//load up the user model
-//need a way to get the Id??? either that or store by email in db
 var User = require('models/User');
-var async = require('async');
 
 function localLoginVerifyCallback(email, password, done) {
-  User.selectByEmail(email, function(err, result) {
-    if (err) {
-      return done(err);
-    }
-    if (!result || !User.utils.isValidPassword(password, result.password)) {
-      return done(
-        null, 
-        false, 
-        { message: 'Invalid email and password combination.' });
-    }
-    done(null, result);
+  User.get(email)
+  process.nextTick(function() {
+    User.findOne(
+      {'local.email': email}, 
+      function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        //no user found
+        if (!user) {
+          return done(null, false, { message: 'No user found.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Oops! Wrong password.' });
+        } else {
+          return done(null, user);
+        }
+      });
   });
 }
 
 function localSignupVerifyCallback(email, password, done) {
-  var checkEmailTaken = function(callback) {
-    User.selectByEmail(email, function(err, result) {
-      if (err) { return callback(err); }
-      if (result) {
-        return done(null, false, {message: 'That email is already taken.'});
-      }
-      callback(null);
-    });
+
+  var findUserCallback = function(err, user) {
+    if (err) {
+      return done(err);
+    }
+
+    // check to see if theres already a user with that email
+    // else create the user
+    if (user) {
+      return done(null, false, {message: 'That email is already taken.'});
+    } else {
+      var newUser = new User();
+      newUser.local.email = email;
+      newUser.local.password = newUser.generateHash(password);
+      newUser.save(function(err) {
+        if (err) { return done(err); }
+        return done(null, newUser);
+      });
+    }
   };
 
-  var createNewUser = function(callback) {
-    User.insert(email, password, function(err) {
-      err ? callback(err) : callback(null);
-    });
-  };
+  process.nextTick(function() {
+    // check if the user is already logged in
+    if (!req.user) {
+      User.findOne({'local.email':  email}, findUserCallback);
+    } else {
+      var user = req.user;
+      user.local.email = email;
+      user.local.password = user.generateHash(password);
+      user.save(function(err) {
+        if (err) { return done(err); }
+        return done(null, user);
+      });
+    }
+  });
+}*/
 
-  //must serialize user by getting full user (including id)
-  var serializeUser = function(callback) {
-    User.selectByEmail(email, function(err, result) {
-      err ? callback(err) : callback(null, result);
-    });
-  }
-
-  async.waterfall(
-    [
-      checkEmailTaken, 
-      createNewUser, 
-      serializeUser
-    ], 
-    function(err, result) {
-      if (err) { return done(err); }
-      done(null, result);
-    });
-}
 
 module.exports = function(passport) {
   /* Passport needs ability to serialize and deserialize users out of session */
--- config/settings/secrets.js
+++ config/settings/secrets.js
@@ -3,7 +3,8 @@ module.exports = {
     host: 'us-cdbr-azure-east-a.cloudapp.net',
     user: 'b6bf084c45dab9',
     password: 'c2bed79d',
-    database: 'iouuomeA14Hvih1b'
+    database: 'iouuomeA14Hvih1b',
+    protocol : 'mysql'
   },
   sessionSecret: process.env.SESSION_SECRET || 'Custom session secret',
   host: process.env.HOST || 'localhost',
